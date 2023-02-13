76594801b674d6014ba823637b2a15345c709424
KarandeepBisht <karandeepbisht@gmail.com>
2023-02-13 11:08:41 +0530

    Block Saturation api

---
+++ api/src/main/java/org/apache/fineract/cn/reporting/api/domain/BlockSaturationRequest.java
@@ -0,0 +1,49 @@
+package org.apache.fineract.cn.reporting.api.domain;
+
+public class BlockSaturationRequest {
+    public String getLocation_type() {
+        return location_type;
+    }
+
+    public void setLocation_type(String location_type) {
+        this.location_type = location_type;
+    }
+
+    public String getDate_to() {
+        return date_to;
+    }
+
+    public void setDate_to(String date_to) {
+        this.date_to = date_to;
+    }
+
+    public String getDate_from() {
+        return date_from;
+    }
+
+    public void setDate_from(String date_from) {
+        this.date_from = date_from;
+    }
+
+    public String getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(String state_id) {
+        this.state_id = state_id;
+    }
+
+    public String getDistrict_id() {
+        return district_id;
+    }
+
+    public void setDistrict_id(String district_id) {
+        this.district_id = district_id;
+    }
+
+    private String location_type;
+    private String date_to;
+    private String date_from;
+    private String state_id;
+    private String district_id;
+}
