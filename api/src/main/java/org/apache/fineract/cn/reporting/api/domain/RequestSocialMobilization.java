b72537190effe93703b7ad0fc33bf879467bc33a
KarandeepBisht <karandeepbisht@gmail.com>
2023-03-21 16:07:04 +0530

    Social mobilization API

---
+++ api/src/main/java/org/apache/fineract/cn/reporting/api/domain/RequestSocialMobilization.java
@@ -0,0 +1,73 @@
+package org.apache.fineract.cn.reporting.api.domain;
+
+public class RequestSocialMobilization {
+    private Integer stateId;
+
+    private Integer districtId;
+
+    public Integer getStateId() {
+        return stateId;
+    }
+
+
+
+    public void setStateId(Integer stateId) {
+        this.stateId = stateId;
+    }
+
+    public Integer getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(Integer districtId) {
+        this.districtId = districtId;
+    }
+
+    public Integer getBlockId() {
+        return blockId;
+    }
+
+    public void setBlockId(Integer blockId) {
+        this.blockId = blockId;
+    }
+
+    public String getYearMonth() {
+        return yearMonth;
+    }
+
+    public void setYearMonth(String yearMonth) {
+        this.yearMonth = yearMonth;
+    }
+
+    public Integer getGeographicalFlag() {
+        return geographicalFlag;
+    }
+
+    public void setGeographicalFlag(Integer geographicalFlag) {
+        this.geographicalFlag = geographicalFlag;
+    }
+
+    private Integer blockId;
+    private String yearMonth;
+    private Integer geographicalFlag;
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    private String fromDate;
+    private String toDate;
+
+}
---
+++ api/src/main/java/org/apache/fineract/cn/reporting/api/domain/ResponseSocialMobilization.java
@@ -0,0 +1,34 @@
+package org.apache.fineract.cn.reporting.api.domain;
+
+public class ResponseSocialMobilization {
+
+    private Integer totalRuralHouseholds;
+    private Integer targetRuralHouseholds;
+    private Integer shgMem;
+
+    public Integer getTotalRuralHouseholds() {
+        return totalRuralHouseholds;
+    }
+
+
+
+    public void setTotalRuralHouseholds(Integer totalRuralHouseholds) {
+        this.totalRuralHouseholds = totalRuralHouseholds;
+    }
+
+    public Integer getTargetRuralHouseholds() {
+        return targetRuralHouseholds;
+    }
+
+    public void setTargetRuralHouseholds(Integer targetRuralHouseholds) {
+        this.targetRuralHouseholds = targetRuralHouseholds;
+    }
+
+    public Integer getShgMem() {
+        return shgMem;
+    }
+
+    public void setShgMem(Integer shgMem) {
+        this.shgMem = shgMem;
+    }
+}
