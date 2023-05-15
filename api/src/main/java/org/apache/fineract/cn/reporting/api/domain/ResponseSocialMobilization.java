321547145c5168c821a3df5011bf8cdfe09df5a9
KarandeepBisht <karandeepbisht@gmail.com>
2023-05-15 17:08:02 +0530

    Block saturation api and added some feilds

--- api/src/main/java/org/apache/fineract/cn/reporting/api/domain/ResponseSocialMobilization.java
+++ api/src/main/java/org/apache/fineract/cn/reporting/api/domain/ResponseSocialMobilization.java
@@ -159,7 +159,6 @@ public class ResponseSocialMobilization {
     private Integer intBlock6to10;
     private Integer intBlockMore10;
     private Integer isSaturated;
-
     private Integer shgObcMonth;
     private Integer shgMemObcMonth;
     private Integer educationUptoGradAndAboveMonth;
@@ -169,6 +168,78 @@ public class ResponseSocialMobilization {
     private Integer monthsOld24to60Month;
     private Integer monthsOld60to120Month;
     private Integer monthsOld120MoreMonth;
+    private Integer shgActivated;
+    private Integer voActivated;
+    private Integer clfActivated;
+    private Integer memActivated;
+    private Integer intBlockZero3sat;
+    private Integer intBlockThree6Sat;
+    private Integer intBlockSix10Sat;
+    private Integer intBlockMore10Sat;
+
+    public Integer getIntBlockZero3sat() {
+        return intBlockZero3sat;
+    }
+
+    public void setIntBlockZero3sat(Integer intBlockZero3sat) {
+        this.intBlockZero3sat = intBlockZero3sat;
+    }
+
+    public Integer getIntBlockThree6Sat() {
+        return intBlockThree6Sat;
+    }
+
+    public void setIntBlockThree6Sat(Integer intBlockThree6Sat) {
+        this.intBlockThree6Sat = intBlockThree6Sat;
+    }
+
+    public Integer getIntBlockSix10Sat() {
+        return intBlockSix10Sat;
+    }
+
+    public void setIntBlockSix10Sat(Integer intBlockSix10Sat) {
+        this.intBlockSix10Sat = intBlockSix10Sat;
+    }
+
+    public Integer getIntBlockMore10Sat() {
+        return intBlockMore10Sat;
+    }
+
+    public void setIntBlockMore10Sat(Integer intBlockMore10Sat) {
+        this.intBlockMore10Sat = intBlockMore10Sat;
+    }
+
+    public Integer getMemActivated() {
+        return memActivated;
+    }
+
+    public void setMemActivated(Integer memActivated) {
+        this.memActivated = memActivated;
+    }
+
+    public Integer getVoActivated() {
+        return voActivated;
+    }
+
+    public void setVoActivated(Integer voActivated) {
+        this.voActivated = voActivated;
+    }
+
+    public Integer getClfActivated() {
+        return clfActivated;
+    }
+
+    public void setClfActivated(Integer clfActivated) {
+        this.clfActivated = clfActivated;
+    }
+
+    public Integer getShgActivated() {
+        return shgActivated;
+    }
+
+    public void setShgActivated(Integer shgActivated) {
+        this.shgActivated = shgActivated;
+    }
 
     public Integer getMonthOld12to24Month() {
         return monthOld12to24Month;
