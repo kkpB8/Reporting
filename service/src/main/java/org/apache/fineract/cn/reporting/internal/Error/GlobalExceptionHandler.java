7fb6c1bbfe9ac19e286b75fd049e6d61e0d21746
Supriya Kumari <Supriya.kumari@microwarecorp.com>
2023-06-07 11:55:57 +0530

    some changes

--- service/src/main/java/org/apache/fineract/cn/reporting/internal/Error/GlobalExceptionHandler.java
+++ service/src/main/java/org/apache/fineract/cn/reporting/internal/Error/GlobalExceptionHandler.java
@@ -21,6 +21,7 @@ package org.apache.fineract.cn.reporting.internal.Error;
 import org.apache.fineract.cn.reporting.internal.exception.CustomStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
@@ -66,12 +67,6 @@ class CustomGlobalExceptionHandler  {
     error.setStatus((HttpStatus.PARTIAL_CONTENT.value()));
     return new ResponseEntity<>(error, HttpStatus.PARTIAL_CONTENT);
   }
-
-  /*@ExceptionHandler(TokenAlreadyFoundException.class)
-  public void springHandleNotTokenAlreadyFound(HttpServletResponse response) throws IOException {
-    response.sendError(609,"You already have a valid token. If your token is lost, you can force Login");
-  }*/
-
 
   @ExceptionHandler(value = LevelReached.class)
   public ResponseEntity<CustomErrorResponse> handleGenericNotFoundException(LevelReached e) {
@@ -129,4 +124,16 @@ class CustomGlobalExceptionHandler  {
     return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 
   }
+  @ExceptionHandler(value = Exception.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  public ResponseEntity<CustomErrorResponse> exception(Exception e) {
+    int startIndex = e.getMessage().indexOf("'") + 1;
+    int endIndex = e.getMessage().indexOf("'", startIndex);
+    String value = e.getMessage().substring(startIndex, endIndex);
+    CustomErrorResponse error = new CustomErrorResponse(CustomStatus.BAD_REQUEST_CODE,value+" must not be null.");
+    error.setTimestamp(LocalDateTime.now().toString());
+    error.setStatus((HttpStatus.BAD_REQUEST.value()));
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  }
+
 }
